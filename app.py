c73e376b1a5853ca8757a6026200f4168990cd6e
yongsin tan yongsin91@hotmail.com <yongsin91@hotmail.com>
2022-07-01 23:25:46 +0800

    preparing streamlit

--- app.py
+++ app.py
@@ -1,9 +1,46 @@
 import streamlit as st
 import joblib
+import requests
+
 
 '''
 # New York Taxi Fare Prediction
 '''
+def coordinates(input_add):
+    """Checks whether the address entered is valid and extracts the
+    coordinates through an API
+    """
+    api_url = 'https://nominatim.openstreetmap.org/search'
+    response = requests.get(api_url, params={'q':input_add,
+                                                'format':'json'}).json()
+
+    new_york = []
+    for ny_address in response:
+        ny_address_split = ny_address['display_name'].split(', ')
+        if ny_address_split[-1] == "United States" and\
+                (ny_address_split[-2] == "New York" or\
+                 ny_address_split[-3] == "New York"):
+            new_york.append(ny_address)
+
+    for add in new_york:
+        if add['display_name'].lower() == input_add:
+            lon = float(add['lon'])
+            lat = float(add['lat'])
+            return lon, lat
+
+    if len(new_york) == 1:
+        st.markdown("*We found one possible address but it doesn't match your query exactly.\
+                Please refine your search or copy this address into the query box:*")
+        st.markdown(f"""`{address['display_name']}`""")
+    elif len(new_york) > 1:
+        st.write("*We found multiple possible addresses. Please refine your search or\
+                copy one of the addresses below into the query box:*")
+        for index, address in enumerate(response[:3]):
+            st.markdown(f"""`{index + 1})  {address['display_name']}`""")
+    elif len(new_york) == 0 and input_add != "":
+        st.write("*No addresses were found. Please refine your search.*")
+
+    return -9999, -9999
 
 st.markdown('''
 The prediction of taxi fare in New York City. The dataset is based on [Kaggle New York Taxi Fare Challenge](https://www.kaggle.com/competitions/new-york-city-taxi-fare-prediction/overview)
@@ -14,17 +51,17 @@ The prediction of taxi fare in New York City. The dataset is based on [Kaggle Ne
 
 1. Date Time
 '''
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 d = col1.date_input("Input Date")
 t = col2.time_input("Input Time")
+a = col3.number_input("Input Number of Passengers", min_value=1, max_value=8)
+# Obtaining coordinates for pickup and dropoff address
+p_add = st.text_input('Input Pickup address').strip().lower()
+p_lon, p_lat = coordinates(p_add)
+d_add = st.text_input('Input dropoff address').strip().lower()
+d_lon, d_lat = coordinates(d_add)
 
-col3, col4 = st.columns(2)
-pickup_lon = col3.number_input("Input Pickup Longitude")
-pickup_lat = col4.number_input("Input Pickup Latitude")
-
-col5, col6 = st.columns(2)
-pickup_lon = col5.number_input("Input Drop off Longitude")
-pickup_lat = col6.number_input("Input Drop off Latitude")
+predict = st.button("Predict Taxi Fare")
 
 model = joblib.load('TaxiFareModel/model.joblib')
 url = 'https://taxifare.lewagon.ai/predict'
